717fc0b034394c9ade59113daa215830cc92dea5
renzon <renzon@gmail.com>
2015-08-23 09:35:44 -0300

    Aula sobre métodos

--- pessoas.py
+++ pessoas.py
@@ -4,6 +4,9 @@ class Pessoa():
     def __init__(self, idade=18):
         self.idade = idade
 
+    def cumprimentar(self, nome):
+        return 'Olá sou o %s, eu tenho %s anos de idade' % (nome, self.idade)
+
 
 renzo = Pessoa(32)
 osmar = Pessoa(27)
@@ -17,8 +20,7 @@ print(renzo.idade)
 print(osmar.idade)
 print(renzo.idade)
 
-
-Pessoa.olhos=3
+Pessoa.olhos = 3
 print(renzo.olhos)
 print(osmar.olhos)
 print(Pessoa.olhos)
@@ -26,3 +28,7 @@ print(id(renzo.olhos))
 print(id(osmar.olhos))
 print(id(Pessoa.olhos))
 
+# print(Pessoa.cumprimentar(renzo))
+renzo.idade = 18
+print(renzo.cumprimentar(nome='Renzo'))
+print(osmar.cumprimentar('Osmar'))
